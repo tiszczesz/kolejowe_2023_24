74e0e29994cfdc09c2f267044fc60a4e7798caf3
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-11 16:06:45 +0200

    hh

--- 4pr_gr1/cw2/cw2.js
+++ 4pr_gr1/cw2/cw2.js
@@ -1,7 +1,25 @@
 const root = document.querySelector("#root");
 
 //utworzyc liste todos
+const todos = [
+    ['zrobić ćwiczenie',true],
+    ['zgłosić zrobione ćwiczenie',false],
+    ['cieszyć się brakiem bańki z aktywności',false]
+];
 //wygenerowac ul za pomocą createElement
+const ul = document.createElement("ul");
 //w petli wygenerowac li za pomocą createElement
-//dodac do ul liste li appendChild
+for(let elem of todos){
+    console.log(elem);
+    const li = document.createElement("li");
+    const text = document.createTextNode(elem[0]);
+    li.appendChild(text);
+    //dodac do ul liste li appendChild
+    ul.appendChild(li);
+}
+todos.forEach((v)=>{
+    console.log(v);}
+    );
+
 //dodac do root calosc
+root.appendChild(ul);
