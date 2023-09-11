bbf67d54d9cdc16094a041e7faf3bd2ab06befd6
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-11 15:07:48 +0200

    gg

---
+++ 4pr_gr1/cw2/cw2.js
@@ -0,0 +1,7 @@
+const root = document.querySelector("#root");
+
+//utworzyc liste todos
+//wygenerowac ul za pomocą createElement
+//w petli wygenerowac li za pomocą createElement
+//dodac do ul liste li appendChild
+//dodac do root calosc
