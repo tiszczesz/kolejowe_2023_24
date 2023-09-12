f41501c897e6bf545dac4efb1967bbd349a3e990
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-12 09:15:23 +0200

    kk

--- 3pb_gr1/cw1/Program.cs
+++ 3pb_gr1/cw1/Program.cs
@@ -2,8 +2,19 @@
 Console.Write("Hello, World!\n");
 Console.WriteLine("Witamy w swiecie dotnet");
 //Console.ReadKey();
-int a = 23;
-int b = 34;
+Console.Write("Podaj a: ");
+
+int a =  Convert.ToInt32(Console.ReadLine());
+Console.Write("Podaj b: ");
+int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(a+" + "+b+" = "+(a+b));
 Console.WriteLine($"{a} + {b} = {a+b}");
+Console.WriteLine($"{a} - {b} = {a-b}");
+Console.WriteLine($"{a} * {b} = {a*b}");
+if(b==0){
+    Console.WriteLine($"{a} / {b} = BRAK WYNIKU!!");
+}else{
+    Console.WriteLine($"{a} / {b} = {(double)a/b}");
+}
+
 
