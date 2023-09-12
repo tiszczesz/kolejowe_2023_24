7718f0212ddef21f84ba89f13b4d8397489d7194
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-12 10:42:26 +0200

    hh

---
+++ 3pb_gr2/cw1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Podaj a: ");
+int a = Convert.ToInt32( Console.ReadLine());
+Console.Write("Podaj b: ");
+int b = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine(a+" + "+b+" = "+(a+b));
+Console.WriteLine($"{a} + {b} = {(a+b)}");
+Console.WriteLine($"{a} - {b} = {(a-b)}");
+Console.WriteLine($"{a} * {b} = {(a*b)}");
+if(b==0){
+Console.WriteLine($"{a} / {b} = BRAK WYNIKU!!!");
+
+}else{
+    Console.WriteLine($"{a} / {b} = {((double)a/b)}");
+}
