611d7d5f008dbc5a462eeeea3f9e00e744fac90f
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-12 08:53:47 +0200

    jj

--- 3pb_gr1/cw1/Program.cs
+++ 3pb_gr1/cw1/Program.cs
@@ -2,3 +2,8 @@
 Console.Write("Hello, World!\n");
 Console.WriteLine("Witamy w swiecie dotnet");
 //Console.ReadKey();
+int a = 23;
+int b = 34;
+Console.WriteLine(a+" + "+b+" = "+(a+b));
+Console.WriteLine($"{a} + {b} = {a+b}");
+
