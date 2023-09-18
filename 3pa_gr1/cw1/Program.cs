ea3ec3a35e3f955066a3eed7c56a71645494d27c
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-18 17:43:23 +0200

    gg

--- 3pa_gr1/cw1/Program.cs
+++ 3pa_gr1/cw1/Program.cs
@@ -1,11 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Witaj swiecie");
-Console.Write("Podaj a: ");
-int a = Convert.ToInt32( Console.ReadLine());
-Console.Write("Podaj b: ");
-int b = Convert.ToInt32( Console.ReadLine());
-Console.WriteLine($"a = {a}");
-Console.WriteLine($"{a} + {b} = {a+b}");
-Console.WriteLine($"{a} - {b} = {a-b}");
-Console.WriteLine($"{a} * {b} = {a*b}");
-Console.WriteLine($"{a} / {b} = {(float)a/b}");
+﻿
+try
+{
+    string firstname = Console.ReadLine();
+    Console.Write("Podaj a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Podaj b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"a = {a}");
+    Console.WriteLine($"{a} + {b} = {a + b}");
+    Console.WriteLine($"{a} - {b} = {a - b}");
+    Console.WriteLine($"{a} * {b} = {a * b}");
+    if (b != 0)
+    {
+        Console.WriteLine($"{a} / {b} = {(float)a / b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} / {b} = BRAK WYNIKU");
+    }
+
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Bledne dane");
+    Console.WriteLine(ex.Message);
+}
