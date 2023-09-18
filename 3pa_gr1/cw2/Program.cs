a9c5a7e9ed8e2bc1767f16d9d046678f8470d473
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-18 18:00:27 +0200

    hh

---
+++ 3pa_gr1/cw2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Podaj tekst: ");
+string text = Console.ReadLine();
+Console.Write("Podaj ilosc powtorzen: ");
+int times = Convert.ToInt32(Console.ReadLine());
+for(int i=0; i<times;i++ ){
+    Console.WriteLine($"i = {i}: {text}");    
+}
+int iterator = 0;
+Console.WriteLine("-------------------------");
+while(iterator<times){
+    Console.WriteLine($"i = {iterator++}: {text}");
+}
