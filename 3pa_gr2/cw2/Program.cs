ff88d26cbacb0a94e6c36f2b109f3f915d2c71b4
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-18 18:50:14 +0200

    gg

---
+++ 3pa_gr2/cw2/Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    Console.Write("Podaj swoje imie: ");
+    string? firstname = Console.ReadLine();
+    Console.Write("Podaj swoj wiek: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Witaj: " + firstname?.ToUpper());
+    if (age >= 18)
+    {
+        Console.WriteLine($"Witaj {firstname} w swiecie doroslych");
+    }
+    else
+    {
+        Console.WriteLine($"Witaj {firstname} niestety to serwis nie dla Ciebie");
+    }
+}catch(FormatException ex){
+    Console.WriteLine("Bledne dane");
+    Console.WriteLine(ex.Message);
+}
+
+
