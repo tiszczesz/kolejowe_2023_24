7338cc1faac38655e90463bbed5580a05563f9e7
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-18 14:10:56 +0200

    gg

--- 4pr_gr1/cw3/cw3.js
+++ 4pr_gr1/cw3/cw3.js
@@ -17,6 +17,32 @@ const Book1 = {
   setPrice: function (price) {
     this.price = price;
   },
+  thisArrow: ()=>{
+    console.log(this);
+  },
+  thisFunction: function(){
+    console.log(this);
+  }
 };
 Book1.setPrice(99.69);
-document.querySelector("#root").innerHTML = Book1.info();
+
+console.log(Book1.thisArrow());
+console.log(Book1.thisFunction());
+////////////////////////////
+function Game(title,price,genre){
+  this.title = title;
+  this.price = price;
+  this.genre = genre;
+  this.show = function(){
+    return `
+      <div>
+      Tytuł: ${this.title} Cena: ${this.price} Rodzaj: ${this.genre}      
+      </div>
+    `
+  };
+}
+const g1 = new Game("Wiedźmin 3",45.99,2015);
+const g2 = new Game("Wiedźmin 4",345.99,2028);
+console.log(g2);
+document.querySelector("#root1").innerHTML = Book1.info();
+document.querySelector("#root2").innerHTML = g2.show();
