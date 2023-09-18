45fe0945eec739b9f93401f35204fa349eac790c
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-18 19:19:02 +0200

    gg

---
+++ 3pa_gr2/cw3/Program.cs
@@ -0,0 +1,46 @@
+﻿void cw0()
+{
+    Console.Write("Podaj a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Podaj b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{a} + {b} = {a + b}");
+    Console.WriteLine($"{a} - {b} = {a - b}");
+    Console.WriteLine($"{a} * {b} = {a * b}");
+    if (b != 0)
+    {
+        Console.WriteLine($"{a} / {b} = {(float)a / b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} / {b} = BRAK WYNIKU");
+    }
+}
+void cw1()
+{
+    Console.Write("Podaj a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Podaj b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{a} + {b} = {a + b}");
+    Console.WriteLine($"{a} - {b} = {a - b}");
+    Console.WriteLine($"{a} * {b} = {a * b}");
+    if (b != 0)
+    {
+        Console.WriteLine($"{a} / {b} = {(float)a / b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} / {b} = BRAK WYNIKU");
+    }
+}
+void cw2()
+{
+    Console.Write("Podaj tekst: ");
+    string? text = Console.ReadLine();
+    Console.Write("Ile razy pwtorzyc: ");
+    int ile = Convert.ToInt32(Console.ReadLine());
+    //for ,while, do-while
+    Console.WriteLine(" ============================================== ");
+}
+cw2();
