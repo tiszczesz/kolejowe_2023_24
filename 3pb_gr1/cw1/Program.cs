7f87d7b0bd8e7b6aa7dcf81552fd30f60ede6d9b
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-19 07:51:57 +0200

    gg

--- 3pb_gr1/cw1/Program.cs
+++ 3pb_gr1/cw1/Program.cs
@@ -1,20 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Hello, World!\n");
-Console.WriteLine("Witamy w swiecie dotnet");
-//Console.ReadKey();
-Console.Write("Podaj a: ");
+﻿try{
+    Console.WriteLine("Witamy w swiecie dotnet");
+    //Console.ReadKey();
+    Console.Write("Podaj a: ");
 
-int a =  Convert.ToInt32(Console.ReadLine());
-Console.Write("Podaj b: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(a+" + "+b+" = "+(a+b));
-Console.WriteLine($"{a} + {b} = {a+b}");
-Console.WriteLine($"{a} - {b} = {a-b}");
-Console.WriteLine($"{a} * {b} = {a*b}");
-if(b==0){
-    Console.WriteLine($"{a} / {b} = BRAK WYNIKU!!");
-}else{
-    Console.WriteLine($"{a} / {b} = {(double)a/b}");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Podaj b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(a + " + " + b + " = " + (a + b));
+    Console.WriteLine($"{a} + {b} = {a + b}");
+    Console.WriteLine($"{a} - {b} = {a - b}");
+    Console.WriteLine($"{a} * {b} = {a * b}");
+    if (b == 0)    {
+        Console.WriteLine($"{a} / {b} = BRAK WYNIKU!!");
+    }
+    else    {
+        Console.WriteLine($"{a} / {b} = {(double)a / b}");
+    }
+}
+catch (FormatException ex){
+    Console.WriteLine("Bledne dane");
+    Console.WriteLine(ex.Message);
 }
 
 
