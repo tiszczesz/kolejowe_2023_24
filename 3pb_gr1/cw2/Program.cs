b011de18bf343cb2de6ef2a7797e35f13018b5c4
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-19 08:22:19 +0200

    hh

---
+++ 3pb_gr1/cw2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Podaj tekst: ");
+string? text = Console.ReadLine();
+Console.Write("Podaj ilosc wykonan: ");
+int times = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Za pomoca petli for(...){....}");
+for(int i=0; i<times;i++ ){    
+    Console.WriteLine($"{text} i = {i}");
+}
+
+//wykorzystac petle while i do-while do identycznego efektu ja petla for
