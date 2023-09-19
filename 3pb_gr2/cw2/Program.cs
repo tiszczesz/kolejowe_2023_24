134c93faada07786857528655d478a703e84643e
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-19 09:53:58 +0200

    ff

--- 3pb_gr2/cw2/Program.cs
+++ 3pb_gr2/cw2/Program.cs
@@ -1,19 +1,37 @@
-﻿Console.Write("Podaj fajny tekst: ");
-string? text = Console.ReadLine();
-Console.Write("Ile razy powtorzyc: ");
-int times = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("petla for(;;){....}");
-for(int i = 0; i<times;i++){    
-    Console.WriteLine($"{text} i = {i}");
+﻿void part1()
+{
+    Console.Write("Podaj fajny tekst: ");
+    string? text = Console.ReadLine();
+    Console.Write("Ile razy powtorzyc: ");
+    int times = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("petla for(;;){....}");
+    for (int i = 0; i < times; i++)
+    {
+        Console.WriteLine($"{text} i = {i}");
+    }
+    Console.WriteLine("petla while(){....}");
+    int j = 0;
+    while (j < times)
+    {
+        Console.WriteLine($"{text} j = {j++}");
+    }
+    j = 0;
+    Console.WriteLine("petla do{....}while()");
+    do
+    {
+        Console.WriteLine($"{text} j = {j++}");
+    } while (j < times);
+
 }
-Console.WriteLine("petla while(){....}");
-int j=0;
-while(j<times){
-    Console.WriteLine($"{text} j = {j++}");
+//part1();
+void part2(){
+    //zgadywanka dla Janka
+    //losujemy liczbe z przedzialu 0..50
+    //a uzytkownik zgaduje podpowiadamy 
+    //czy za duzo czy za malo
+    //zliczamy ilosc prob
+    Random rnd = new Random();
+    int szukana = rnd.Next(0,50);
 }
-j=0;
-Console.WriteLine("petla do{....}while()");
-do{
-    Console.WriteLine($"{text} j = {j++}");
-}while(j<times);
+part2();
 
