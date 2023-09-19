dc68e2c7a3b2b52e8716c3244b5fcfe421e91f23
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-19 10:47:17 +0200

    gg

---
+++ 3pb_gr2/cw3/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] imiona = { "Anna", "Joanna", "Tomasz" };
+foreach (var item in imiona)
+{
+    Console.WriteLine(item);
+}
+imiona[imiona.Length - 1] = "Roman";
+Console.WriteLine($"Rozmiar: {imiona.Length}");
+foreach (var item in imiona)
+{
+    Console.WriteLine(item);
+}
+int[] losowe = new int[10];
+//funkcja ktora wypelni losowymi  0..100 wartosciami ta tablice
+void FillArray(int[] contener)
+{
+
+}
+void ShowArray(int[] contener)
+{
+    foreach (var item in contener)
+    {
+        Console.WriteLine(item);
+    }
+}
+FillArray(losowe);
+ShowArray(losowe);
+int max = getMax(losowe);
