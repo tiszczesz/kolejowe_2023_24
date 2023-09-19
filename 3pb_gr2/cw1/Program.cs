aec75f586351c81fbe73172194d52988cfa1d6c9
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-19 09:22:34 +0200

    gg

--- 3pb_gr2/cw1/Program.cs
+++ 3pb_gr2/cw1/Program.cs
@@ -1,16 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Podaj a: ");
-int a = Convert.ToInt32( Console.ReadLine());
-Console.Write("Podaj b: ");
-int b = Convert.ToInt32( Console.ReadLine());
-Console.WriteLine(a+" + "+b+" = "+(a+b));
-Console.WriteLine($"{a} + {b} = {(a+b)}");
-Console.WriteLine($"{a} - {b} = {(a-b)}");
-Console.WriteLine($"{a} * {b} = {(a*b)}");
-if(b==0){
-Console.WriteLine($"{a} / {b} = BRAK WYNIKU!!!");
-
-}else{
-    Console.WriteLine($"{a} / {b} = {((double)a/b)}");
+﻿try
+{
+    Console.Write("Podaj a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Podaj b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(a + " + " + b + " = " + (a + b));
+    Console.WriteLine($"{a} + {b} = {(a + b)}");
+    Console.WriteLine($"{a} - {b} = {(a - b)}");
+    Console.WriteLine($"{a} * {b} = {(a * b)}");
+    if (b == 0)
+    {
+        Console.WriteLine($"{a} / {b} = BRAK WYNIKU!!!");
+    }
+    else
+    {
+        Console.WriteLine($"{a} / {b} = {((double)a / b)}");
+    }
 }
+catch (FormatException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
