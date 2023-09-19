a2e91e89c55283f071b883ebe0b57e8d742c12b0
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-19 09:44:11 +0200

    gg

---
+++ 3pb_gr2/cw2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Podaj fajny tekst: ");
+string? text = Console.ReadLine();
+Console.Write("Ile razy powtorzyc: ");
+int times = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("petla for(;;){....}");
+for(int i = 0; i<times;i++){    
+    Console.WriteLine($"{text} i = {i}");
+}
+Console.WriteLine("petla while(){....}");
+Console.WriteLine("petla do{....}while()");
