67265733bf3f9455714013af11ff67d28c8320d6
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-19 08:27:09 +0200

    dd

--- 3pb_gr1/cw2/Program.cs
+++ 3pb_gr1/cw2/Program.cs
@@ -6,5 +6,14 @@ Console.WriteLine("Za pomoca petli for(...){....}");
 for(int i=0; i<times;i++ ){    
     Console.WriteLine($"{text} i = {i}");
 }
-
+Console.WriteLine("Za pomoca petli while(...){....}");
+int j = 0;
+while(j<times ){    
+    Console.WriteLine($"{text} j = {j++}");
+}
 //wykorzystac petle while i do-while do identycznego efektu ja petla for
+Console.WriteLine("Za pomoca petli do{....}while(...)");
+j = 0;
+do{    
+    Console.WriteLine($"{text} j = {j++}");
+}while(j<times);
