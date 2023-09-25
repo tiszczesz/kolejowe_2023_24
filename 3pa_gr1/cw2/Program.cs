04220022fddf97abcc9ebfe0498ca47562e3d084
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-25 16:33:58 +0200

    gg

--- 3pa_gr1/cw2/Program.cs
+++ 3pa_gr1/cw2/Program.cs
@@ -2,11 +2,17 @@
 string text = Console.ReadLine();
 Console.Write("Podaj ilosc powtorzen: ");
 int times = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("--------petla for------------");
 for(int i=0; i<times;i++ ){
     Console.WriteLine($"i = {i}: {text}");    
 }
 int iterator = 0;
-Console.WriteLine("-------------------------");
+Console.WriteLine("--------petla while------------");
 while(iterator<times){
     Console.WriteLine($"i = {iterator++}: {text}");
 }
+Console.WriteLine("petla do while");
+iterator = 0;
+do{
+    Console.WriteLine($"i = {iterator++}: {text}");
+} while(iterator<times);
