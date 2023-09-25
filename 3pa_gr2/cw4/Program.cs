bdb47c0f9d43fce06a970984e049ecd488b3ab54
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-25 19:39:25 +0200

    gg

--- 3pa_gr2/cw4/Program.cs
+++ 3pa_gr2/cw4/Program.cs
@@ -1,22 +1,73 @@
-﻿int[] tab = new int[10];
-showTab(tab);
+﻿
+void ex1()
+{
+    int[] tab = new int[10];
+    showTab(tab);
 
-tab[6] = 666;
-//tab[10] = 7777;
-showTab(tab);
-fillTab(tab);
-showTab(tab);
+    tab[6] = 666;
+    //tab[10] = 7777;
+    showTab(tab);
+    fillTab(tab);
+    showTab(tab);
+}
+void ex2()
+{
+    Console.Write("Podaj rozmiar tablicy: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] numbers = new int[size];
+    fillTab(numbers);
+    showTab(numbers);
+    Console.WriteLine($"Max element: {getMax(numbers)}");
+    Console.WriteLine($"Min element: {getMin(numbers)}");
+    Console.WriteLine($"Max element: {numbers.Max()}");
+    Console.WriteLine($"Min element: {numbers.Min()}");
+    string[] tt = {"dsfsdfs","ewrwerwr","retreterte","erwerwr"};
+    showTab<string>(tt);
+   // Console.WriteLine($"suma: {numbers.Sum()}");
+   // Console.WriteLine($"srednia: {numbers.Average()}");
 
+}
+
+int getMin(int[] numbers)
+{
+    if (numbers.Length <= 0) throw new FormatException(message: "Pusty zbior");
+    int min = numbers[0];
+    foreach (var e in numbers)
+    {
+        if (e < min) min = e;
+    }
+    return min;
+}
+
+int getMax(int[] numbers)
+{
+    if (numbers.Length <= 0) throw new FormatException(message: "Pusty zbior");
+    int max = numbers[0];
+    foreach (var e in numbers)
+    {
+        if (e > max) max = e;
+    }
+    return max;
+
+}
+
+//ex1();
+ex2();
 //Console.WriteLine(tab.Length);
-void showTab(int[] t){
-    foreach (int elem in t)    {
+void showTab<T>(T[] t)
+{
+    foreach (T elem in t)
+    {
         Console.Write(elem + " ");
     }
     Console.WriteLine();
 }
-void fillTab(int[] t){
+void fillTab(int[] t)
+{
     Random rnd = new Random();
-    for(int i=0; i<t.Length;i++){
-        t[i] = rnd.Next(0,200);
+    for (int i = 0; i < t.Length; i++)
+    {
+        t[i] = rnd.Next(-10, 200);
     }
+
 }
