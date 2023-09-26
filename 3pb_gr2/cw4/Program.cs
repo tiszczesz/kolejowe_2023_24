474ea2cf9976dac89afe45ed3b3577c68b21cc5e
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-26 10:45:06 +0200

    gg

---
+++ 3pb_gr2/cw4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string[] words = { "dom", "chleb", "cukier", "ryba", "kon" };
+//string[] sorted = sort(words);
+string[] sorted2 = words.OrderBy(w => w).ToArray();
+
+string[] sort(string[] words)
+{
+    throw new NotImplementedException();
+}
+
+double[] numbers = { 12, 67, 90.8, 56, 78 };
+print<string>(words);
+print<double>(numbers);
+void print<T>(T[] tab)
+{
+    foreach (T elem in tab)
+    {
+        Console.Write(elem + " ");
+    }
+    Console.WriteLine();
+}
