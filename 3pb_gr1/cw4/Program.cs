4045ccae877e2566746f260a89da918555026c52
tiszczesz <tiszczesz@users.noreply.github.com>
2023-09-26 09:18:53 +0200

    gg

--- 3pb_gr1/cw4/Program.cs
+++ 3pb_gr1/cw4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words = {"dom","szkola","to","ryba"};
+string[] sort = words.OrderBy(x=>x).ToArray();
+double[] numbers = {23.5, 45.8, 78,89.99};
+print<string>(words);
+print<string>(sort);
+print<double>(numbers);
+if (string.Compare(words[0],words[1])<0)
+{
+    Console.WriteLine("gggg");
+}
+
+
+void print<T>(T[] t)
+{
+    foreach (T elem in t)
+    {
+        Console.Write(elem + " ");
+    }
+    Console.WriteLine($" Rozmiar tablicy: {t.Length}");
+}
--- 3pb_gr2/cw3/Program.cs
+++ 3pb_gr2/cw3/Program.cs
@@ -1,30 +1,43 @@
-﻿string[] imiona = { "Anna", "Joanna", "Tomasz" };
-foreach (var item in imiona)
+﻿void Ex1()
 {
-    Console.WriteLine(item);
-}
-imiona[imiona.Length - 1] = "Roman";
-Console.WriteLine($"Rozmiar: {imiona.Length}");
-foreach (var item in imiona)
-{
-    Console.WriteLine(item);
-}
-int[] losowe = new int[10];
-//funkcja ktora wypelni losowymi  0..100 wartosciami ta tablice
-void FillArray(int[] contener)
-{
-
-}
-void ShowArray(int[] contener)
-{
-    foreach (var item in contener)
+    string[] imiona = { "Anna", "Joanna", "Tomasz" };
+    foreach (var item in imiona)
+    {
+        Console.WriteLine(item);
+    }
+    imiona[imiona.Length - 1] = "Roman";
+    Console.WriteLine($"Rozmiar: {imiona.Length}");
+    foreach (var item in imiona)
     {
         Console.WriteLine(item);
     }
 }
-int getMax(int[] data){
-    return 0;
+void Ex2()
+{
+    int[] losowe = new int[10];
+    //funkcja ktora wypelni losowymi  0..100 wartosciami ta tablice
+
+    FillArray(losowe);
+    ShowArray(losowe);
+    int max = getMax(losowe);
+    void FillArray(int[] contener)
+    {
+
+    }
+    void ShowArray(int[] contener)
+    {
+        foreach (var item in contener)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    int getMax(int[] data)
+    {
+        return 0;
+    }
 }
-FillArray(losowe);
-ShowArray(losowe);
-int max = getMax(losowe);
+//Ex1();
+Ex2();
+
+
+
