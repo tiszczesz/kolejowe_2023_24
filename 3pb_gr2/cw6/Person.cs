7aef4efb43dd7ae834561f3a70e126742e20b730
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-03 10:48:31 +0200

    gg

---
+++ 3pb_gr2/cw6/Person.cs
@@ -0,0 +1,17 @@
+namespace cw6;
+
+public class Person{
+    //pola
+    private string? firstname;
+    private string? lastname;
+    //metody
+    public Person()
+    {
+        
+    }
+    //preciazanie metod
+    public Person(string firstname,string lastname)
+    {
+        
+    }
+}
---
+++ 3pb_gr2/cw6/Program.cs
@@ -0,0 +1,7 @@
+﻿using cw6;
+
+Person p1 = new Person();
+Console.WriteLine(p1.GetHashCode());
+Console.WriteLine(p1.ToString());
+Console.WriteLine(p1.GetType());
+
