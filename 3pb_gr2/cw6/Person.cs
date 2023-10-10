39166a1c9a8ab63487eab71570662f9e5991a58c
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-10 09:49:21 +0200

    gg

--- 3pb_gr2/cw6/Person.cs
+++ 3pb_gr2/cw6/Person.cs
@@ -1,17 +1,28 @@
 namespace cw6;
 
-public class Person{
+public class Person
+{
     //pola
     private string? firstname;
     private string? lastname;
+
+    private DateTime birthDay;
     //metody
     public Person()
     {
-        
+        firstname = "noname";
+        lastname = "noname";
+        birthDay = new DateTime(2000, 1, 1);
     }
-    //preciazanie metod
-    public Person(string firstname,string lastname)
+    // //preciazanie metod
+    public Person(string firstname,string lastname,DateTime birthDay)
     {
-        
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthDay = birthDay;
+    }
+    public override string ToString()
+    {
+        return $"Imie: {firstname} Nazwisko: {lastname} data urodzenia: {birthDay}";
     }
 }
--- 3pb_gr2/cw6/Program.cs
+++ 3pb_gr2/cw6/Program.cs
@@ -1,7 +1,8 @@
 ﻿using cw6;
 
 Person p1 = new Person();
+Person p2 = new Person("Adam","Nowak",new DateTime(2006,12,23));
 Console.WriteLine(p1.GetHashCode());
 Console.WriteLine(p1.ToString());
-Console.WriteLine(p1.GetType());
+Console.WriteLine(p2.ToString());
 
