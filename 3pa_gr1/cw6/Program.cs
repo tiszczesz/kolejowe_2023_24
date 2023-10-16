8e6ee76b5d24ce825c76ef5a76e8e1bcdb485995
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-16 18:03:22 +0200

    hh

---
+++ 3pa_gr1/cw6/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Ex1();
+Ex2();
+void Ex2(){
+    //uzytkownik proszony jest o podawanie tekstu liniami
+    //az do uzycia napisu null lub Null lub nULL
+    // raczej do ramu List a potem do pliku jedna komenda
+    //wtedy konczy i zapisuje do pliku
+    //nastepnie pyta czy wczytac plik i go wyswietlic
+    // jesli odpowiadamy y to to robi lub konczy program
+    File.ReadAllLines("dane.txt"); 
+
+}
+void Ex1()
+{
+    List<string> words = new List<string>{
+    "ala ma kota",
+    "rybki ...",
+    "cos odkrywczego"
+};
+    File.WriteAllLines("dane.txt", words);
+}
