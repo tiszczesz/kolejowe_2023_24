c615162eb8dfaeb8b54e56dd0421c66a2acdfcfb
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-17 08:12:34 +0200

    ggg

---
+++ 3pb_gr1/cw7/Program.cs
@@ -0,0 +1,29 @@
+﻿Ex1();
+
+void ShowData(IEnumerable<string>  wiadro)
+{
+    foreach (string line in wiadro)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+Ex2();
+void Ex2(){
+    //uzytkownik wpisuje z klawiatury teksty liniami
+    //tak dlugo az napisze koniec
+    //wtedy jest zapis do pliku z listy
+    //ewentulnie wyswietlenie odczytanego pliku
+}
+void Ex1()
+{
+    List<string> texts = new List<string>{
+    "Dzisiaj zdjecia",
+    "jeszcze tylko 2h ze sz...",
+    "a z osiem",
+    "cos nowego do wiadra"
+};
+    File.WriteAllLines("dane.txt", texts);//zapis
+    var wiadro = File.ReadAllLines("dane.txt");//odczyt
+    ShowData(wiadro);
+}
