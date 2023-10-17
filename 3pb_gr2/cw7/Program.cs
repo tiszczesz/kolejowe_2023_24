21094ac6386639a7fcef146e2fa2a6955862a380
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-17 09:48:23 +0200

    hh

---
+++ 3pb_gr2/cw7/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Ex1();
+Ex2();
+
+void Ex2()
+{
+   //napisac aplikacje generujaca liczby losowe --> List<int>
+   // ilosc linii jest ustalona przez uzytkownika
+   // kazda linia zapisywana do List lub Array
+   // potem zapis do pliku wszystkich linii 
+   // kazda linia na koncu po znaku | ma sume liczb
+   // np
+   // 2 4 2|8
+   // 2|2
+   //ilosc liczb w linii jest losowa (np do wylosowania ZERO)
+}
+static void Ex1()
+{
+    var data = new List<string>{
+    "linia pierwsza",
+    "to nie jest nowosc",
+    "prawie ostania linia",
+    "fdg dfgdfg dfgdg dg "
+};
+    File.WriteAllLines("data.txt", data);//zapis do pliku
+    var wiadro = File.ReadAllLines("data.txt");//odczyt z pliku
+    foreach (var line in wiadro)
+    {
+        Console.WriteLine(line);
+    }
+}
