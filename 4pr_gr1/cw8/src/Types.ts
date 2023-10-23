601ddc86c51aca6fc3d7b9121f0e79829f3d371c
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-23 13:14:34 +0200

    jj

--- 4pr_gr1/cw8/src/Types.ts
+++ 4pr_gr1/cw8/src/Types.ts
@@ -4,7 +4,20 @@ export interface Todo{
     title: string,
     completed: boolean
 }
+export interface User{    
+        id: number,
+        name: string,
+        username: string,
+        email: string,
+       
+        phone: string,
+        website: string,   
+}
 export async function getTodos():Promise<Todo[]> {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    return response.json();    
+}
+export async function getUsers():Promise<User[]> {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     return response.json();    
 }
--- 4pr_gr1/cw8/src/index.ts
+++ 4pr_gr1/cw8/src/index.ts
@@ -1,5 +1,7 @@
-import { Todo, getTodos } from "./Types.js";
+import { Todo,User,getUsers, getTodos } from "./Types.js";
 let todos: Todo[];
-
+let users: User[];
 todos = await getTodos();
+users = await getUsers();
 console.log(todos);
+console.log(users);
