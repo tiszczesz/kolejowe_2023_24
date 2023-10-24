ebb373ed23f09c5fc792ff594a0c56673ce2ca23
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-24 08:56:59 +0200

    gg

---
+++ 3pb_gr1/cw8/Fake.cs
@@ -0,0 +1,24 @@
+﻿namespace cw8;
+
+public class Fake
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }    
+    public decimal Price { get; set; }
+    public string SerializeToCSV(){
+        return $"{Id};{Name};{Price}\n";
+    }
+    public static Fake? DeserializeFromCSV(string line){
+        var data = line.Split(';');
+        if(data.Length!=3) return null;
+        try{
+            return new Fake{
+            Id = Convert.ToInt32(data[0]),
+            Name = data[1],
+            Price = Convert.ToDecimal(data[2])
+        };
+        }catch(FormatException ex){
+            return null;
+        }        
+    }
+}
