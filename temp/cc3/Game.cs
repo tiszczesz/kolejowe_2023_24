0830d47a20808b56737a1bf8ed74d650216a9303
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-24 09:52:59 +0200

    gg

---
+++ temp/cc3/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace cc3;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public decimal Price { get; set; }
+    public DateTime Edition { get; set; }
+    public override string ToString()
+    {
+        return $"Id: {Id} Title: {Title} Price: {Price} Edition: {Edition}";
+    }
+}
--- temp/cc3/Program.cs
+++ temp/cc3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using cc3;
+
+List<Game> games = new List<Game>{
+     new Game{Id=1,Title="Wiedzmin",Price=66.90m,Edition=new DateTime(2008,12,9)},
+     new Game{Id=2,Title="Rymok",Price=66.90m,Edition=new DateTime(2012,12,9)},
+     new Game{Id=3,Title="Cyberpunk 2077",Price=166.90m,Edition=new DateTime(2019,12,9)},
+     new Game{Id=4,Title="FIFA 2019",Price=67.90m,Edition=new DateTime(2017,3,19)}
+};
+List<string> lines = new List<string>();
+ foreach(Game g in games){
+    lines.Add(JsonSerializer.Serialize(g));
+}
+File.WriteAllLines("games.txt",lines);
+List<string> deserialize = File.ReadAllLines("games.txt").ToList();
+
+var g1 = JsonSerializer.Deserialize<Game>(deserialize[2]);
+Console.WriteLine(g1);
