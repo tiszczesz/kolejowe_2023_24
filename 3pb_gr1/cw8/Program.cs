dfeef912f3b6625d97afdf394e19f180a221f3df
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-24 08:45:40 +0200

    gg

--- 3pb_gr1/cw8/Program.cs
+++ 3pb_gr1/cw8/Program.cs
@@ -11,19 +11,59 @@ void Menu()
         Console.WriteLine("\t1 - Dopisz do pliku");
         Console.WriteLine("\t2 - Wyswietl zawartosc pliku");
         Console.WriteLine("\t3 - Usun plik");
+        Console.WriteLine("\t4 - Usun linie tekstu");
         Console.WriteLine("\n\tk - Zakoncz program");
 
         Console.Write("\n\t\tWybierz opcje ---->  ");
         string? input = Console.ReadLine()?.Trim();
-        option = (input!=null && input.Length>0) ? input.ToLower()[0]: ' ';
-        switch(option){
-            case '1': InsertText();break;
-            case '2': ShowAll();break;
-            case '3': RemoveFile();break;
+        option = (input != null && input.Length > 0) ? input.ToLower()[0] : ' ';
+        switch (option)
+        {
+            case '1': InsertText(); break;
+            case '2': ShowAll(); break;
+            case '3': RemoveFile(); break;
+            case '4': RemoveLine(); break;
         }
     } while (option != 'k');
 
 
+}
+
+void RemoveLine()
+{
+    if (!File.Exists(FILENAME))
+    {
+        Console.WriteLine("  BRAK PLIKU!!!!!");
+        return;
+    }
+    List<string> data = File.ReadAllLines(FILENAME).ToList();
+    if (data.Count == 0)
+    {
+        Console.WriteLine("  PLIK PUSTU!!!!!");
+        return;
+    }
+    Console.Write($"Podaj numer lini do usuniecia (1,{data.Count})");
+    //todo try catch
+    try
+    {
+        int numer = Convert.ToInt32(Console.ReadLine());
+        if (numer < 1 || numer > data.Count)
+        {
+            Console.WriteLine("  Bledny numer lini!!!!!");
+            return;
+        }
+        data.RemoveAt(numer - 1);
+        if (data.Count > 0)
+        {
+            File.WriteAllLines(FILENAME, data);
+        }
+        else
+        {
+            File.Delete(FILENAME);
+        }
+    }catch(FormatException ex){
+        Console.WriteLine("ERROR: "+ex.Message);
+    }
 }
 
 void RemoveFile()
@@ -33,13 +73,15 @@ void RemoveFile()
 
 void ShowAll()
 {
-    if(!File.Exists(FILENAME)){
+    if (!File.Exists(FILENAME))
+    {
         Console.WriteLine("  BRAK PLIKU!!!!!");
         return;
     }
     var data = File.ReadAllLines(FILENAME);
     int count = 0;
-    foreach(var line in data){
+    foreach (var line in data)
+    {
         count++;
         Console.WriteLine($"Linia tekstu nr {count}: {line}");
     }
@@ -49,7 +91,8 @@ void InsertText()
 {
     Console.Write("Podaj tekst: ");
     string? line = Console.ReadLine();
-    if(line!=null && line.Trim().Length>0){
-        File.AppendAllText(FILENAME,line+Environment.NewLine);
+    if (line != null && line.Trim().Length > 0)
+    {
+        File.AppendAllText(FILENAME, line + Environment.NewLine);
     }
 }
