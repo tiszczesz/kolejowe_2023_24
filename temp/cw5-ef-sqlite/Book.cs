10cb403bb4754e326fb7961692440df2b5cfd0a8
tiszczesz <tiszczesz@users.noreply.github.com>
2023-10-30 18:41:21 +0100

    gg

---
+++ temp/cw5-ef-sqlite/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace cw5_ef_sqlite;
+
+public class Book
+{
+    public int Id { get; set; } 
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ temp/cw5-ef-sqlite/BookDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cw5_ef_sqlite;
+
+public class BookDbContext:DbContext
+{
+    private string connString = "Data Source=books.db";
+    public BookDbContext()
+    {
+        
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlite(connString);
+    }
+    public DbSet<Book> Books { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Book>().HasData(
+            new Book{
+                Id = 1,
+                Title = "ABC programowania w c#",
+                Author = "Antoni Bałecki",
+                Price = 23.78m
+            },
+             new Book{
+                Id = 2,
+                Title = "JavaScript versus TypeScript",
+                Author = "Bożena Gryk",
+                Price = 99.78m
+            },
+             new Book{
+                Id = 3,
+                Title = "Robotyka",
+                Author = "Antoni Bałecki",
+                Price = 67.78m
+            }
+        );
+    }
+}
