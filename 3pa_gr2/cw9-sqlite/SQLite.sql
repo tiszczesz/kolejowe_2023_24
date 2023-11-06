835a3fac20f07847eaea6c66d31c89b8e7e34d77
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-06 18:37:58 +0100

    gg

---
+++ 3pa_gr2/cw9-sqlite/SQLite.sql
@@ -0,0 +1,20 @@
+-- SQLite
+CREATE TABLE Posts(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    content VARCHAR(50),
+    date DATE 
+)
+
+INSERT INTO Posts(title,content,date)
+VALUES("post 1","To jest pierwsy post"
+    ,"2023-09-12");
+INSERT INTO Posts(title,content,date)
+VALUES("post 2","Lepiej pisać dla własnego dobra"
+    ,"2023-11-06");
+INSERT INTO Posts(title,content,date)
+VALUES("post 3","Jest głośno ale mam sposoby na wyciszenia"
+    ,"2023-11-06");    
+SELECT 
+strftime('%Y', date)
+ FROM Posts;
