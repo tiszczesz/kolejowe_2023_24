dd9a2b346acb3af24df074332e77aae6def2ab81
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-06 19:41:02 +0100

    gg

---
+++ 3pa_gr2/cw9-sqlite/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace cw9_sqlite;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Conent { get; set; }
+    public DateTime? CreateDate { get; set; }
+}
---
+++ 3pa_gr2/cw9-sqlite/PostsRepo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace cw9_sqlite;
+
+public class PostsRepo
+{
+    private string connString;
+    public PostsRepo()
+    {
+        connString = "Data Source=data.db";
+    }
+    public List<Post> GetPosts(){
+        List<Post> posts = new List<Post>();
+        SqliteConnection conn = new SqliteConnection(connString);
+        SqliteCommand command = conn.CreateCommand();
+        command.CommandText = "SELECT * FROM Posts";
+        SqliteDataReader rd = command.ExecuteReader();
+        if(rd.HasRows){
+            while(rd.Read()){
+                posts.Add(new Post{
+                    Id = rd.GetInt32(0),
+                    Title = rd.GetString(1),
+                    Conent = rd.GetString(3),
+                    
+                });
+            }
+        }
+        conn.Open();
+
+
+
+        conn.Close(); 
+        return posts;
+    }
+}
