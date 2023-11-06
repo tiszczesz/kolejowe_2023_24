d946019a373a67f8e3b8b716c542a144af2de5c6
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-06 17:06:27 +0100

    hh

---
+++ 3pa_gr1/cw8/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace cw8;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Genre { get; set; }
+    public int? Year { get; set; }
+    public decimal? Price { get; set; }
+}
---
+++ 3pa_gr1/cw8/GemesRepo.cs
@@ -0,0 +1,13 @@
+﻿namespace cw8;
+
+public class GamesRepo
+{
+    private string connString;
+    public GamesRepo()
+    {
+        
+    }
+    public List<Game> GetGames(){
+        List<Game> gemes = new List<Game>();
+    }
+}
