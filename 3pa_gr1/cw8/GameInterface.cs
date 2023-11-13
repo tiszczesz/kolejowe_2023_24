a7f98e00d9c7f2ce86b64598dab0384cc209320a
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-13 16:47:07 +0100

    gg

--- 3pa_gr1/cw8/GameInterface.cs
+++ 3pa_gr1/cw8/GameInterface.cs
@@ -2,16 +2,24 @@
 
 public class GameInterface
 {
-    public static Game CreateGame(){
+    public static Game CreateGame()
+    {
         Console.WriteLine("Dodawanie nowej gry");
         Console.Write("Podaj tytul gry: ");
         var title = Console.ReadLine()?.Trim();
-
-
-        return new Game{
+        Console.Write("Podaj rodzaj gry: ");
+        var genre = Console.ReadLine()?.Trim();
+        Console.Write("Podaj rok powstania gry: ");
+        var year = Convert.ToInt32(Console.ReadLine()?.Trim());
+        Console.Write("Podaj cene gry: ");
+        var price = Convert.ToDecimal(Console.ReadLine()?.Trim());
+        return new Game
+        {
             Id = -1,
             Title = title,
-            //.....
+            Genre = genre,
+            Year = year,
+            Price = price            
         };
 
     }
