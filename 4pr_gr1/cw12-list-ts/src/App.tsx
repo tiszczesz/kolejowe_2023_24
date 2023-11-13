38263a7db38d916229a300167b966ddd3b6651da
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-13 15:19:53 +0100

    ggg

--- 4pr_gr1/cw12-list-ts/src/App.tsx
+++ 4pr_gr1/cw12-list-ts/src/App.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Users } from "./users";
 import "./App.css";
 import Message from "./components/Message";
+import BookImage from './media/book1.png'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <div>
           <h1>Lista użytkowników ilość: {Users.length}</h1>
           {Users.map((elm, k) => (
-            <Message key={k} user={elm} />
+            <Message key={k} user={elm} userImage={BookImage} />
           ))}
         </div>
       ) : (
--- 4pr_gr1/cw12-list-ts/src/components/Message.tsx
+++ 4pr_gr1/cw12-list-ts/src/components/Message.tsx
@@ -3,19 +3,23 @@ import './Message.css';
 import { User } from '../users';
 
 type MessageProps = {
-    user:User
+    user:User,
+    userImage:string
 }
 
 const Message = (props: MessageProps) => {
   return (
     <div style={{margin:"10px"}}>
+         <img src={props.userImage} alt="book" />
         <h1>Wizytówka użytkownika</h1>
         <h4>Imię: <span className='info'>
             {props.user.firstname}</span></h4>
         <h4>Nazwisko: <span className='info'>
             {props.user.lastname}</span></h4>
         <h5>Wiek: <span className='info'>
-            {props.user.age}</span></h5>
+            {props.user.age}</span>
+           
+            </h5>
         <hr />
     </div>
   )
