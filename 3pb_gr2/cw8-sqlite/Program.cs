35ff12581a9b80cb48b7fd253d80e08dc85edc91
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-21 09:24:37 +0100

    gg

--- 3pb_gr2/cw8-sqlite/Program.cs
+++ 3pb_gr2/cw8-sqlite/Program.cs
@@ -7,3 +7,8 @@ var posts = repo.GetPosts();
 ConsoleInter.ShowAllPosts(posts);
 var p = ConsoleInter.GetPost();
 repo.InsertPost(p);
+//dodac usuwanie posta po podaniu przez uzytkownika tytulu posta
+// i potwierdzeniu ze wlasnie ten post chce usunac ---> 3
+// dodac update wybranego posta po tytule
+// uzytkownik decyduje co ma updatowac   ----> 4
+// pelna aplikacja z menu jak we wczesniejszym projekcie  -----> 6
