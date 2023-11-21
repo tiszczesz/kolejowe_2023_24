078375ec92de37384b7bdbe5b078322f758cdc1a
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-21 07:55:15 +0100

    hh

--- 3pb_gr1/cw9/Program.cs
+++ 3pb_gr1/cw9/Program.cs
@@ -6,3 +6,7 @@ var students = repo.GetStudents();
 StudentsInterface.ShowAllStudents(students);
 var s = StudentsInterface.GetStudent();
 repo.InsertStudent(s);
+//dorobic uzytkownik pyta o nazwisko wyszukiwanie w List<Student> 
+//potem potwierdzenie usuwania studenta ---> 3
+// update studenta o danym nazwisku --->4
+// pelna aplikacja CRUD z menu podobnie jak filmy
