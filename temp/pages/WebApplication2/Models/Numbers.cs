2040d0b2eb39a082931e49089c44344a1e1dc7ae
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-11-22 09:42:16 +0100

    rr

---
+++ temp/pages/WebApplication2/Models/Numbers.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models; 
+
+public class Numbers {
+    public List<int> GetRandomNumbers(int count) {
+        Random rnd = new Random();
+        List<int> numbers = new List<int>();
+        for (int i = 0; i < count; i++) {
+            numbers.Add(rnd.Next(0,100));
+        }
+        return numbers;
+    }
+}
--- temp/pages/WebApplication2/Program.cs
+++ temp/pages/WebApplication2/Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using WebApplication2.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
@@ -9,7 +12,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 app.MapRazorPages();
-// app.MapGet("/", () => "Hello World!");
+ app.MapGet("/", () => {
+     Numbers numbers = new Numbers();
+     StringBuilder sb = new StringBuilder();
+     foreach (var number in numbers.GetRandomNumbers(20)) {
+         sb.Append(number.ToString()).Append(" ");
+     }
+     return sb.ToString();
+ });
 // app.MapGet("/api", () => "API");
 
 app.Run();
