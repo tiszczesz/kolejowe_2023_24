480e6c37c8f16a508176aac0a6d5841542b25688
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-27 16:23:10 +0100

    gg

--- temp/rrr/c1/src/App.tsx
+++ temp/rrr/c1/src/App.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyRange from './components/MyRange';
+import MyRadio from './components/MyRadio';
 
 function App() {
   const [valueRed, setValueRed] = useState("255");
   const [valueGreen, setValueGreen] = useState("255");
   const [valueBlue, setValueBlue] = useState("255");
+  const [radioValue,setRadioValue] = useState("");
   document.body.style.backgroundColor = `rgb(${valueRed},${valueGreen},${valueBlue})`;
   return (
     <div >
@@ -20,6 +22,12 @@ function App() {
     handleChange={(value: string) => setValueBlue(value)} intValue={255} 
     name={'blue'} />
     <span>Range: {valueRed},{valueGreen}, {valueBlue}</span>
+
+    <div>
+      <MyRadio data={["232323","wrwrwrwr","etetetetet"]} name='ddd' initVal='' 
+      handleChanges={(value:string)=>{setRadioValue(value)}} />
+      <span>Zmienione radio: {radioValue}</span>
+    </div>
     </div>
   );
 }
