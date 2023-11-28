bde875fbc424e6ebd6ee90868c6a467d2f140bd8
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-28 08:30:48 +0100

    gg

---
+++ 3pb_gr1/cw10/cw10_empty/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+string firstname = "Tomasz";
+int age = 34;
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/p1", () => "Witaj ze strony p1");
+app.MapGet("/p2", () =>
+ $"Witaj {firstname} twój wiek: {age} Dzisiaj mamy: {DateTime.Now.ToShortDateString()}");
+
+app.Run();
