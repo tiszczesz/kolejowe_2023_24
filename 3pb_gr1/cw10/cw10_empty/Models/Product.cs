ff06129c07ec36d2eddfa8bc8aa5fc4b5679c177
tiszczesz <tiszczesz@users.noreply.github.com>
2023-11-28 08:38:46 +0100

    dd

--- 3pb_gr1/cw10/cw10_empty/Models/Product.cs
+++ 3pb_gr1/cw10/cw10_empty/Models/Product.cs
@@ -5,5 +5,5 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; }    
     public decimal Price { get; set; }
-    public DateTime ProdDate { get; set; }
+    public DateTime ProdDate { get; set; } = DateTime.Now;
 }
