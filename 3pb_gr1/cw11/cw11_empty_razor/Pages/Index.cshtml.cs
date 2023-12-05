8a7790b13953ece26891e3965de0aa16796f7a01
tiszczesz <tiszczesz@users.noreply.github.com>
2023-12-05 09:24:34 +0100

    gg

--- 3pb_gr1/cw11/cw11_empty_razor/Pages/Index.cshtml.cs
+++ 3pb_gr1/cw11/cw11_empty_razor/Pages/Index.cshtml.cs
@@ -5,8 +5,11 @@ namespace MyApp.Namespace
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public List<string> Colors { get; set; }
         public void OnGet()
         {
+            Colors = new List<string>{"red","green","blue","yellow"};
         }
     }
 }
