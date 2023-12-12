afba5a87e0a23caa17ff1881d2a8268616ea3220
tiszczesz <tiszczesz@users.noreply.github.com>
2023-12-12 08:59:21 +0100

    gg

--- 3pb_gr1/cw12/cw12_empty/Pages/Holliday.cshtml.cs
+++ 3pb_gr1/cw12/cw12_empty/Pages/Holliday.cshtml.cs
@@ -1,10 +1,18 @@
+using cw12_empty;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace
 {
+   
     public class HollidayModel : PageModel
     {
+        [BindProperty]
+        public List<Gift> Gifts { get; set; }
+        public HollidayModel()
+        {
+            Gifts = GiftRepo.GetGifts();
+        }
         public void OnGet()
         {
         }
