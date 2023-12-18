0a131edbf7ec4fe0c4cf8647ad6861c6d9dc0365
tiszczesz <tiszczesz@users.noreply.github.com>
2023-12-18 18:45:30 +0100

    ff

--- 3pa_gr2/cw13/cw13-layout/Program.cs
+++ 3pa_gr2/cw13/cw13-layout/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapRazorPages();
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
