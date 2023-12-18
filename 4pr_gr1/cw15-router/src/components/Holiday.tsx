fd08210dcc0ba8c451e836c687ba6396a2e8cab7
tiszczesz <tiszczesz@users.noreply.github.com>
2023-12-18 14:56:23 +0100

    Update Holiday.tsx

--- 4pr_gr1/cw15-router/src/components/Holiday.tsx
+++ 4pr_gr1/cw15-router/src/components/Holiday.tsx
@@ -1,17 +1,20 @@
 import { Gifts } from "../models/gifts"
 import choinka from '../media/choinka.png'
+import { useState } from "react"
 type Props = {}
 
 const Holiday = (props: Props) => {
+  const [gift,setGift] = useState("");
   return (
     <div>
         <h3>Lista prezentów</h3>
         <img src={choinka} alt="choinka" style={{float:"right"}} />
         <ol>
-            {Gifts.map((e,i)=>(
-                <li key={i}>{e}</li>
+            {Gifts.map((item,i)=>(
+                <li style={{cursor:"pointer"}} onClick={(e)=>setGift((e.target as HTMLOListElement).innerText)} key={i}>{item}</li>
             ))}
         </ol>
+        {gift!=="" && <div className="p-2 bg-warning rounded-2">{gift}</div>}
     </div>
 
   )
