3d07d3054ba36cd1d37e4b391a298762c1668a53
tiszczesz <tiszczesz@users.noreply.github.com>
2023-12-18 19:01:43 +0100

    ff

--- 3pa_gr2/cw13/cw13-layout/Pages/Index.cshtml.cs
+++ 3pa_gr2/cw13/cw13-layout/Pages/Index.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace
 {
+
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public List<string> Carols { get; set; }
+        public IndexModel(){
+            Carols = new List<string>{"asdasdsada ","wewqerr","wrwrwrwrwrw"};
+        }
         public void OnGet()
         {
         }
