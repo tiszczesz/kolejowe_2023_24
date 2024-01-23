182f0d3bcd76e8687fd4571e249206a350ec25ac
tiszczesz <tiszczesz@users.noreply.github.com>
2024-01-23 10:19:32 +0100

    ff

---
+++ 3pb_gr2/cw13/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Info { get; set; }
+
+        [BindProperty]
+        public string Result { get; set; }
+        public void OnGet()
+        {
+            ViewData["info2"] = "Ala już kota nie ma";
+            Info = "Ala ma kota";
+            var request = Request;
+        }
+        public void OnPost(){
+             var request = Request;
+             var firstname = Request.Form["firstname"];
+             var lastname = Request.Form["lastname"];
+             Result = "witaj: "+firstname+ " "+lastname;
+        }
+    }
+}
