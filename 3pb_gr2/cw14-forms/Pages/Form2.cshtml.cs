28f65534f3c59e21514c6958bf21e22761f4b4d7
tiszczesz <tiszczesz@users.noreply.github.com>
2024-01-30 10:23:27 +0100

    gg

---
+++ 3pb_gr2/cw14-forms/Pages/Form2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class Form2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
