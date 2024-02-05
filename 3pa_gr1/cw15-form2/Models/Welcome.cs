fc3b65a6d88d676cb8fe0011506bc31ecd370752
tiszczesz <tiszczesz@users.noreply.github.com>
2024-02-05 18:13:01 +0100

    tt

--- 3pa_gr1/cw15-form2/Models/Welcome.cs
+++ 3pa_gr1/cw15-form2/Models/Welcome.cs
@@ -1,13 +1,23 @@
-﻿namespace cw15_form2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cw15_form2.Models
 {
-	public class Welcome
-	{
-        public string Name { get; set; }
-        public string Content { get; set; }
-        public DateTime DateGo { get; set; }
-		public override string ToString()
-		{
-			return $"{Name}|{Content}|{DateGo}";	
-		}
-	}
+    public class Welcome
+    {
+        [Display(Name = "Twoja nazwa")]
+        [Required(ErrorMessage = "Musisz się przedstawić")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Co przyniesiesz")]
+        public string? Content { get; set; }
+
+        [Display(Name = "Do kiedy potwirdzenie")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Podaj datę")]
+        public DateTime? DateGo { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}|{Content}|{DateGo?.ToShortDateString()}" + Environment.NewLine;
+        }
+    }
 }
--- 3pa_gr1/cw15-form2/Pages/WelcomePage.cshtml.cs
+++ 3pa_gr1/cw15-form2/Pages/WelcomePage.cshtml.cs
@@ -1,12 +1,25 @@
+using cw15_form2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace cw15_form2.Pages
 {
-    public class WelcomePageModel : PageModel
-    {
+	public class WelcomePageModel : PageModel
+	{
+        [BindProperty]
+        public Welcome	MyWelcome { get; set; }
         public void OnGet()
-        {
-        }
-    }
+		{
+		}
+		public IActionResult OnPost()
+		{
+			if(ModelState.IsValid)
+			{
+			//	var result = MyWelcome;
+				FileRepo.AddToFile(MyWelcome);
+				return RedirectToPage("./Index");
+			}
+			return Page();
+		}
+	}
 }
