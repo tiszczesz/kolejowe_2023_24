0a0b0dc952e1f14ea961a9563e7e33ff36516293
tiszczesz <tiszczesz@users.noreply.github.com>
2024-02-06 09:05:09 +0100

    ff

---
+++ 3pb_gr1/cw16-form2/Models/FileRepo.cs
@@ -0,0 +1,9 @@
+﻿namespace cw16_form2.Models
+{
+    public class FileRepo
+    {
+        public static void AddToFile(Person person,string fileName="data.txt") {
+            File.AppendAllText(fileName,person.ToString());
+        }
+    }
+}
