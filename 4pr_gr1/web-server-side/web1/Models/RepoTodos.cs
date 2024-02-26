1d3a478c8c9ed57f2629295d204d5becd540a93f
tiszczesz <tiszczesz@users.noreply.github.com>
2024-02-26 15:20:36 +0100

    ff

---
+++ 4pr_gr1/web-server-side/web1/Models/RepoTodos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace web1;
+
+public class RepoTodos
+{
+    private string? _connString;
+    public RepoTodos(IConfiguration configuration)
+    {
+        _connString = configuration.GetConnectionString("sqliteconn");
+    }
+    public List<Todo> GetTodos()
+    {
+        List<Todo> todos = new();
+        SqliteConnection connection = new SqliteConnection(_connString);
+        string sql = "SELECT id,content,date FROM todos";
+        SqliteCommand command = connection.CreateCommand();
+        command.CommandText = sql;
+        connection.Open();
+        SqliteDataReader rd = command.ExecuteReader();
+        if (rd.HasRows)
+        {
+            while (rd.Read())
+            {
+                todos.Add(new Todo
+                {
+                    Id = rd.GetInt32(0),
+                    Content = rd.GetString(1),
+                    Date = DateOnly.FromDateTime(rd.GetDateTime(2))
+                });
+            }
+        }
+        connection.Close();
+        return todos;
+    }
+}
--- 4pr_gr1/web-server-side/web1/Pages/Index.cshtml.cs
+++ 4pr_gr1/web-server-side/web1/Pages/Index.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using web1;
 
 namespace MyApp.Namespace
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public List<Todo> Todos { get; set; }
+        private RepoTodos _repo;
+        public IndexModel(IConfiguration configuration)
+        {
+            _repo = new RepoTodos(configuration);
+        }
         public void OnGet()
         {
+            Todos = _repo.GetTodos();
         }
     }
 }
