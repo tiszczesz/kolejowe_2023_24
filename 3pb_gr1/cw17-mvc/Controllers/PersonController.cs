cd5eeb78a7f6416025ca1b7e9e7d035ccf4e9b5e
tiszczesz <tiszczesz@users.noreply.github.com>
2024-03-12 08:51:02 +0100

    gg

---
+++ 3pb_gr1/cw17-mvc/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using cw17_mvc.Models;
+
+namespace cw17_mvc.Controllers;
+
+public class PersonController : Controller {
+    public IActionResult Index(){
+        var person = new Person{
+            Firstname = "Roman",
+            Lastname = "Bułecki",
+            Age = 23
+        };
+        return View(person);
+    }
+}
---
+++ 3pb_gr1/cw17-mvc/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace cw17_mvc.Models;
+
+public class Person
+{
+    public string? Firstname { get; set; }
+    public string? Lastname { get; set; }
+    public int Age { get; set; }
+}
