d31e7c1b234db960e7d19670bf9b8e17a830e879
tiszczesz <tiszczesz@users.noreply.github.com>
2024-03-12 10:14:05 +0100

    hh

---
+++ 3pb_gr2/cw16-mvc/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using cw16_mvc.Models;
+
+namespace cw16_mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult MyAction(){
+        string text = "To jest wykorzystanie modelu";
+        return View("MyAction",  text);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
