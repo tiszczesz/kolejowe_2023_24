8f339ab8c762fa8db93adeb86c5d5cb8cc81269f
tiszczesz <tiszczesz@users.noreply.github.com>
2024-03-25 15:48:58 +0100

    ff

---
+++ temp/json-test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var dane = File.ReadAllText("dane.json");
+var reciples = JsonSerializer.Deserialize<List<Recipe>>(dane);
+Console.WriteLine(reciples.Count);
---
+++ temp/json-test/Recipe.cs
@@ -0,0 +1,7 @@
+public class Recipe{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public List<string>? Ingredient { get; set; }
+    public decimal Price { get; set; }
+    public string? Img { get; set; }
+}
