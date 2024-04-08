6a296dbc6f135bbd252615b7358ce951899cae02
tiszczesz <tiszczesz@users.noreply.github.com>
2024-04-08 19:34:21 +0200

    ff

--- 3pa_gr2/cw17-mvc/Controllers/SilentController.cs
+++ 3pa_gr2/cw17-mvc/Controllers/SilentController.cs
@@ -1,12 +1,21 @@
+using System.Text.Json;
 using cw17_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw17_mvc.Controllers;
 
 
-public class SilentController : Controller{
-    public IActionResult AllStudents(){
-        List<Student> students = new List<Student>();
-        return View(students);
+public class SilentController : Controller
+{
+    public IActionResult AllStudents()
+    {
+        List<Student> students = GetStudents() ?? new List<Student>();
+        return View("Index",students);
+    }
+    private List<Student>? GetStudents()
+    {
+        string dane = System.IO.File.ReadAllText("dane.json");
+        List<Student>? students = JsonSerializer.Deserialize<List<Student>>(dane);
+        return students;
     }
 }
--- 3pa_gr2/cw17-mvc/Models/Student.cs
+++ 3pa_gr2/cw17-mvc/Models/Student.cs
@@ -1,4 +1,8 @@
 namespace cw17_mvc.Models;
 public class Student{
-
+    public int Id { get; set; }
+    public string? Firstname { get; set; }
+    public string? Lastname { get; set; }
+    public double Average { get; set; }
+    public string? Topic { get; set; }
 }
