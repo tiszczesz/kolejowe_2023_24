36db930040b7daa3250c50a2af65cd9fe7773cb2
tiszczesz <tiszczesz@users.noreply.github.com>
2024-04-16 09:42:54 +0200

    jj

---
+++ 3pb_gr2/cw18-sqlite/Controllers/FilmController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cw18_sqlite.Controllers;
+
+public class FilmController:Controller
+{
+    public FilmController(IConfiguration configuration)
+    {
+        
+    }
+    public ActionResult List(){
+        return View();
+    }
+}
--- 3pb_gr2/cw18-sqlite/Program.cs
+++ 3pb_gr2/cw18-sqlite/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+// app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Film}/{action=List}/{id?}");
 
 app.Run();
