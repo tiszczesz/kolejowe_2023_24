5827e01accfd1df813ac02fef55e0a15bd742f46
tiszczesz <tiszczesz@users.noreply.github.com>
2024-04-16 10:22:33 +0200

    gg

---
+++ 3pb_gr2/cw18-sqlite/Models/Film.cs
@@ -0,0 +1,11 @@
+﻿namespace cw18_sqlite.Models;
+
+public class MyFilm
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Director { get; set; }
+    public string? Language { get; set; }
+    public string? Date { get; set; }
+    public decimal? Price { get; set; }
+}
---
+++ 3pb_gr2/cw18-sqlite/Models/FilmsRepo.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace cw18_sqlite.Models;
+
+public class FilmsRepo
+{
+    private readonly string _connString;
+    public FilmsRepo(IConfiguration configuration)
+    {
+        _connString = configuration.GetConnectionString("sqlite") 
+           ?? "Data Source=DbFilms.db";
+    }
+    public List<MyFilm> GetAllFilms(){
+        List<MyFilm> films = new();
+        using (SqliteConnection conn = new SqliteConnection(_connString))
+        {
+            SqliteCommand command = conn.CreateCommand();
+            command.CommandText = "SELECT * FROM Films";
+            conn.Open();
+            SqliteDataReader rd = command.ExecuteReader();
+            while (rd.Read()){
+                films.Add( new MyFilm{
+                    Id = rd.GetInt32(0),
+                    Title = rd.GetString(1),
+                    Director = rd.GetString(2),
+                    Language = rd.GetString(3),
+                    Date = rd.GetString(4),
+                    Price = rd.GetDecimal(5)
+                });
+            }
+        }
+        return films;
+    }
+}
