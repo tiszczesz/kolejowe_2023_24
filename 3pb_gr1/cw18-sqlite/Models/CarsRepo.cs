edf90aae4969cbb460650b858864824835e320f3
pszczesz <pszczesz@gmail.com>
2024-04-23 08:58:24 +0200

    kk

--- 3pb_gr1/cw18-sqlite/Models/CarsRepo.cs
+++ 3pb_gr1/cw18-sqlite/Models/CarsRepo.cs
@@ -42,4 +42,27 @@ public class CarsRepo
         conn.Close();
        }
     }
+
+    public Car? GetCarById(int? id) {
+        Car car;
+        using (SqliteConnection connection = new SqliteConnection(_connString))
+        {
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = $"SELECT * FROM MyCars WHERE id={id}";
+            connection.Open();
+            SqliteDataReader rd = command.ExecuteReader();
+
+            rd.Read();
+            car = new Car {
+                Id = rd.GetInt32(0),
+                Model = rd.GetString(1),
+                Year = rd.GetInt32(2),
+                Price = rd.GetInt32(3)
+            };
+
+        }
+
+        return car;
+
+    }
 }
