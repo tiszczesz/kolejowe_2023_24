c6a911fd2b07abc2255410a5fec41980e63a33d3
tiszczesz <tiszczesz@users.noreply.github.com>
2024-05-27 13:50:16 +0200

    hh

--- 4pr_gr1/ark2/cw-ark1/src/App.tsx
+++ 4pr_gr1/ark2/cw-ark1/src/App.tsx
@@ -1,10 +1,16 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css';
 
 function App() {
+  const courses = [
+    "Programowanie w c#",
+    "Angular dla początkujących",
+    "Kurs Django"
+  ];
   return (
     <>
+    <h2>Liczba kursów: </h2>
     </>
   );
 }
